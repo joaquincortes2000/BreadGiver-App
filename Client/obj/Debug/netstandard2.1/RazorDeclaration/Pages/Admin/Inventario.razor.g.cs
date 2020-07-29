da95e0e2b1b932fcc790d819645395cf4b4407dd
Joaquin <joaquincorteskha@gmail.com>
2020-07-29 19:42:06 -0400

    Commit antes de update
    Pre update commit

--- Client/obj/Debug/netstandard2.1/RazorDeclaration/Pages/Admin/Inventario.razor.g.cs
+++ Client/obj/Debug/netstandard2.1/RazorDeclaration/Pages/Admin/Inventario.razor.g.cs
@@ -91,13 +91,20 @@ using Microsoft.AspNetCore.Components.Authorization;
 #nullable disable
 #nullable restore
 #line 12 "C:\Users\joaqu\Documents\GitHub\BreadGiver-app\Client\_Imports.razor"
-using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
 #line 13 "C:\Users\joaqu\Documents\GitHub\BreadGiver-app\Client\_Imports.razor"
+using Microsoft.AspNetCore.Components;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 14 "C:\Users\joaqu\Documents\GitHub\BreadGiver-app\Client\_Imports.razor"
 using Repositorios;
 
 #line default
