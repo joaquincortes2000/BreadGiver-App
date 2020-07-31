a2f4759e512871d0c64ae3594782c0ecf2749d13
Joaquin <joaquincorteskha@gmail.com>
2020-07-31 14:04:59 -0400

    ultimo commit
    Ultimo commit

--- Shared/Producto.cs
+++ Shared/Producto.cs
@@ -1,11 +1,23 @@
-﻿namespace BreadGiverApp.Shared
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BreadGiverApp.Shared
 {
     public class Producto
     {
         public int Id { get; set; }
+        
+        [Required]
+        [Range(typeof(string), "true", "true",
+        ErrorMessage = "El nombre debe ser solo texto")]
         public string NombreProducto { get; set; }
+        
+        [Range(1, 100000000, ErrorMessage = "El precio no debe ser menor a 0.")]
         public string PrecioProducto { get; set; }
+
+        [Range(1, 100000000, ErrorMessage = "La cantidad de stock no puede ser menor a 0.")]
         public int CantidadProducto { get; set; }
+        
+        
         public string ImagenProducto { get; set; }
 
     }
